e3b31729b4d95256d69afcecf76222ad6f371003
Taner Özer <tanerozer16@gmail.com>
2026-03-17 14:22:51 +0300

    posts added on serverjs

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
+const postRoutes = require("./routes/posts");
 
 const connectDB = require("./config/db");
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("SoCial App API running");
