43a5fafcabe8c517eac3be212dcdfcfdd6a84ed5
Taner Özer <tanerozer16@gmail.com>
2026-03-18 16:08:30 +0300

    Comment Model added

---
+++ api/models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+
+    postId: {
+      type: String,
+      required: true,
+    },
+
+    desc: {
+      type: String,
+      required: true,
+      max: 300,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
