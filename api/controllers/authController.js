0f04c5f892a66c58251d1c0e697b43eca8c97a4b
Taner Özer <tanerozer16@gmail.com>
2026-03-18 16:18:23 +0300

    auth controller edited

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -24,31 +24,38 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const user = await User.findOne({
-      email: req.body.email,
-    });
+    const { email, password } = req.body;
 
-    if (!user) return res.status(404).json("User not found");
+    const user = await User.findOne({ email });
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password,
-    );
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
 
-    if (!validPassword) return res.status(400).json("Wrong password");
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json("Wrong password");
+    }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
 
-    const { password, ...others } = user._doc;
+    const { password: _, ...userData } = user._doc;
 
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        secure: false,
+        sameSite: "lax",
       })
       .status(200)
-      .json(others);
+      .json({
+        message: "Login successful",
+        token,
+        user: userData,
+      });
   } catch (err) {
     res.status(500).json(err);
   }
