a5b0f76de82df388d3b1877cf265e4f41aad64cd
Taner Özer <tanerozer16@gmail.com>
2026-03-18 16:22:12 +0300

    server js edited

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
 
 const connectDB = require("./config/db");
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.get("/", (req, res) => {
   res.send("SoCial App API running");
