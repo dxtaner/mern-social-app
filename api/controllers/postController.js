28b9d2b9bb0b9df5d532a9513d4e663ed03d24e6
Taner Özer <tanerozer16@gmail.com>
2026-04-01 14:17:27 +0300

    post controller edited

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -89,6 +89,24 @@ exports.getFeed = async (req, res) => {
   }
 };
 
+exports.getPostById = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post Not Found" });
+    }
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({
+      message: "An error occurred whilst retrieving the post",
+      error: err.message,
+    });
+  }
+};
+
 exports.getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find()
