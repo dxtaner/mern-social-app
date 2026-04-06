a59a4e263907233169f44a5a2bef56c68fc2ed88
Taner Özer <tanerozer16@gmail.com>
2026-04-06 13:53:01 +0300

    auth controller edited

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,24 +1,59 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const sendEmail = require("../utils/sendEmail");
 
 exports.register = async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
+    const { username, email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const emailToken = crypto.randomBytes(32).toString("hex");
 
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      username,
+      email,
       password: hashedPassword,
+      emailToken,
     });
 
-    const user = await newUser.save();
+    await newUser.save();
 
-    res.status(201).json(user);
+    const verifyUrl = `http://localhost:5173/verify-email/${emailToken}`;
+
+    await sendEmail(
+      email,
+      "Email Doğrulama",
+      `Hesabını doğrulamak için linke tıkla:\n${verifyUrl}`,
+    );
+
+    res.status(201).json({
+      message: "Kayıt başarılı. Email gönderildi.",
+    });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
+  }
+};
+
+exports.verifyEmail = async (req, res) => {
+  try {
+    const user = await User.findOne({ emailToken: req.params.token });
+
+    if (!user) {
+      return res.status(400).json("Geçersiz token");
+    }
+
+    user.isVerified = true;
+    user.emailToken = null;
+
+    await user.save();
+
+    res.json("Email doğrulandı 🎉");
+  } catch (err) {
+    res.status(500).json(err.message);
   }
 };
 
@@ -27,16 +62,14 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
+    if (!user) return res.status(404).json("User not found");
 
-    if (!user) {
-      return res.status(404).json("User not found");
+    if (!user.isVerified) {
+      return res.status(400).json("Email doğrulanmamış");
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json("Wrong password");
-    }
+    if (!isMatch) return res.status(400).json("Wrong password");
 
     user.lastLogin = new Date();
     await user.save();
@@ -60,7 +93,59 @@ exports.login = async (req, res) => {
         user: userData,
       });
   } catch (err) {
-    console.error("Login error:", err);
-    res.status(500).json(err);
+    res.status(500).json(err.message);
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json("User not found");
+
+    const resetToken = crypto.randomBytes(32).toString("hex");
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpire = Date.now() + 1000 * 60 * 15;
+
+    await user.save();
+
+    const resetUrl = `http://localhost:5173/reset-password/${resetToken}`;
+
+    await sendEmail(
+      email,
+      "Şifre Sıfırlama",
+      `Şifreni sıfırlamak için linke tıkla:\n${resetUrl}`,
+    );
+
+    res.json("Reset mail gönderildi 📧");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: req.params.token,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json("Token geçersiz veya süresi dolmuş");
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = null;
+    user.resetPasswordExpire = null;
+
+    await user.save();
+
+    res.json("Şifre başarıyla güncellendi 🔐");
+  } catch (err) {
+    res.status(500).json(err.message);
   }
 };
