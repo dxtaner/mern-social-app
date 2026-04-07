0ae962875a1aa30b9b77109c8bb9d757b9660875
Taner Özer <tanerozer16@gmail.com>
2026-04-07 13:47:17 +0300

    api services edited

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://localhost:8800/api",
-  withCredentials: true,
+});
+
+API.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+
+  return config;
 });
 
 export default API;
